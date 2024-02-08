45f00456e431d3b7717fac588a59c1cdea76f9d4
Swastik <swastikdeshmukh7@gmail.com>
2024-02-08 23:44:06 +0530

    final

--- main.go
+++ main.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 	"sync"
 	"time"
+	"strings"
+	
 )
-
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber
+}
 const conferenceTickets int = 50
 
 var conferenceName = "Go Conference"
